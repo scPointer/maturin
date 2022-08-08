56271aa8a7e3044bcc17d3d041bf6609bc25658e
Luoyuan Xiao <xiaoluoyuan@163.com>
2022-08-08 23:32:03 +0800

    run sh

--- kernel/src/testcases.rs
+++ kernel/src/testcases.rs
@@ -8,10 +8,11 @@ pub const TESTCASES: &[&str] = &[
     // 测 lua 或者 busybox 的时候**不要**打开 base_info，内核输出非常多
 
     //"busybox sh lua_testcode.sh", // lua 测例，已通过
-    "busybox sh busybox_testcode.sh", // busybox 测例
+    //"busybox sh busybox_testcode.sh", // busybox 测例
     //"busybox ls",
+    "busybox sh",
 
-    /* // 很少一点 libc 测例。完整评测见 ./file/test.rs 中，需要把其中 TESTCASES_ITER 和 TEST_STATUS 的值换掉
+    /* // 很少一点 libc 测例。完整评测见 kernel/src/file/device/test.rs 中，需要把其中 TESTCASES_ITER 和 TEST_STATUS 的值换掉
     //"./runtest.exe -w entry-dynamic.exe argv",
     //"./runtest.exe -w entry-dynamic.exe tls_init",
     //"./runtest.exe -w entry-dynamic.exe tls_local_exec",
