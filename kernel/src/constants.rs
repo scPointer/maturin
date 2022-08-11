95147e9cb854a31ae5c41e0ba3a74733edcb24be
cl-a11y <jmgcs65536@gmail.com>
2022-08-11 22:27:41 +0800

    expand disk space to 128m

--- kernel/src/constants.rs
+++ kernel/src/constants.rs
@@ -44,7 +44,7 @@ pub const PHYS_VIRT_OFFSET: usize = 0xFFFF_FFFF_0000_0000;
 /// 表示内存的地址段由此开始
 pub const PHYS_MEMORY_OFFSET: usize = 0x8000_0000;
 /// 表示内存的地址段到此为止
-pub const PHYS_MEMORY_END: usize = 0x8800_0000;
+pub const PHYS_MEMORY_END: usize = 0x9f00_0000;
 
 /// 入口用户程序。OS启动后，只会启动以此为名的用户程序。
 /// 一般来说，这个程序会通过 fork / exec 启动终端和其他程序
@@ -70,7 +70,7 @@ pub const IS_TEST_ENV: bool = true;
 pub const IS_PRELOADED_FS_IMG: bool = false;
 /// 文件系统镜像的大小。注意这个量和 fs-init 模块中 `/src/main.rs` 里生成镜像时的大小相同。
 /// 启动时会从 .data 段加载加载
-const LOCAL_FS_IMG_SIZE: usize = 16 * 2048 * 512; // 16MB
+const LOCAL_FS_IMG_SIZE: usize = 0x1000_0000; // 128MB
 /// 测试时的文件系统镜像大小。
 /// 注意因为这个文件太大，默认是已经被qemu加载好了，启动时不会加载
 const TEST_FS_IMG_SIZE: usize = 0x4000_0000; // 1GB
@@ -81,7 +81,7 @@ pub const FS_IMG_SIZE: usize = if IS_PRELOADED_FS_IMG {
     LOCAL_FS_IMG_SIZE
 };
 /// 设备(sdcard)映射到内存的起始位置
-pub const DEVICE_START: usize = 0x9000_0000;
+pub const DEVICE_START: usize = 0xa000_0000;
 /// 设备映射到内存的最后位置
 pub const DEVICE_END: usize = DEVICE_START + FS_IMG_SIZE;
 
