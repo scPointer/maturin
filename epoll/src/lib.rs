fcdc4855c06e534c8495b0a9fd92d361a5980df9
UNIDY2002 <UNIDY2002@outlook.com>
2022-11-08 19:47:52 +0800

    Add a test case along-side crate `epoll`

--- epoll/src/lib.rs
+++ epoll/src/lib.rs
@@ -12,10 +12,10 @@ mod epoll_file;
 mod flags;
 
 use alloc::sync::Arc;
-pub use flags::{EpollEvent, EpollEventType, EpollCtl};
+pub use epoll_file::EpollFile;
+pub use flags::{EpollCtl, EpollEvent, EpollEventType};
 use syscall::ErrorNo;
 use task_trampoline::{get_file, manually_alloc_type, push_file};
-pub use epoll_file::EpollFile;
 
 /// 执行 epoll_create 系统调用
 ///
@@ -75,4 +75,133 @@ pub fn sys_epoll_wait(epfd: i32, event: *mut EpollEvent, _maxevents: i32, timeou
         }
     }
     Ok(ret_events.len())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        sys_epoll_create, sys_epoll_ctl, sys_epoll_wait, EpollCtl, EpollEvent, EpollEventType,
+    };
+    use alloc::sync::Arc;
+    use alloc::vec::Vec;
+    use base_file::File;
+    use lock::Mutex;
+
+    struct MyTaskTrampoline;
+
+    static mut FILES: Vec<Option<Arc<dyn File>>> = Vec::new();
+
+    impl task_trampoline::TaskTrampoline for MyTaskTrampoline {
+        fn suspend_current_task(&self) {}
+
+        fn get_file(&self, fd: usize) -> Option<Arc<dyn File>> {
+            unsafe {
+                if fd >= FILES.len() || FILES[fd].is_none() {
+                    None
+                } else {
+                    Some(FILES[fd].as_ref().unwrap().clone())
+                }
+            }
+        }
+
+        fn push_file(&self, file: Arc<dyn File>) -> Result<usize, u64> {
+            unsafe {
+                let fd = FILES.len();
+                FILES.push(Some(file));
+                Ok(fd)
+            }
+        }
+
+        fn manually_alloc_user_str(&self, buf: *const u8, len: usize) -> Result<(), u64> {
+            Ok(())
+        }
+
+        fn manually_alloc_range(&self, start_vaddr: usize, end_vaddr: usize) -> Result<(), u64> {
+            Ok(())
+        }
+
+        fn raw_time(&self) -> (usize, usize) {
+            (0, 0)
+        }
+
+        fn raw_timer(&self) -> (usize, usize) {
+            (0, 0)
+        }
+
+        fn set_timer(
+            &self,
+            timer_interval_us: usize,
+            timer_remained_us: usize,
+            timer_type: usize,
+        ) -> bool {
+            true
+        }
+    }
+
+    struct FakeFileInner {
+        ready: bool,
+    }
+
+    struct FakeFile {
+        pub inner: Arc<Mutex<FakeFileInner>>,
+    }
+
+    impl FakeFile {
+        fn set_ready(&self, ready: bool) {
+            let inner = &mut self.inner.lock();
+            inner.ready = ready;
+        }
+    }
+
+    impl File for FakeFile {
+        fn read(&self, buf: &mut [u8]) -> Option<usize> {
+            None
+        }
+
+        fn write(&self, buf: &[u8]) -> Option<usize> {
+            None
+        }
+
+        fn ready_to_read(&self) -> bool {
+            self.inner.lock().ready
+        }
+    }
+
+    #[test]
+    fn epoll_test() {
+        task_trampoline::init_task_trampoline(&MyTaskTrampoline);
+
+        let fake_file = FakeFile {
+            inner: Arc::new(Mutex::new(FakeFileInner { ready: true })),
+        };
+        let fake_fd = task_trampoline::push_file(Arc::new(fake_file)).unwrap();
+
+        let epoll_fd = sys_epoll_create(1).unwrap();
+        let mut e = EpollEvent {
+            events: EpollEventType::EPOLLIN,
+            data: 0,
+        };
+        assert_eq!(
+            sys_epoll_ctl(epoll_fd as i32, 1, fake_fd as i32, &e).unwrap(),
+            0
+        );
+
+        let fake_file = task_trampoline::get_file(fake_fd).unwrap();
+        let fake_file = fake_file
+            .as_any()
+            .downcast_ref::<FakeFile>()
+            .unwrap()
+            .clone();
+        fake_file.set_ready(true);
+
+        assert_eq!(sys_epoll_wait(epoll_fd as i32, &mut e, 1, 0).unwrap(), 1);
+        assert_eq!(sys_epoll_wait(epoll_fd as i32, &mut e, 1, 0).unwrap(), 1);
+    }
+}
+
+#[cfg(test)]
+mod timer {
+    pub fn get_time_ms() -> usize {
+        0
+    }
 }
