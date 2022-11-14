b6deadfb67a5aa97396d6489a90b5c1ade819ec4
闭浩扬 <bhy18@mails.tsinghua.edu.cn>
2022-11-14 11:38:29 +0800

    add more doc

--- range-action-map-test/src/main.rs
+++ range-action-map-test/src/main.rs
@@ -1,3 +1,13 @@
+//! 对 `range-action-map` 的用户态测试。
+//! 请使用 `cargo test` 而非 `cargo run` 进行测试。
+//! 
+//! 目前外部的测试只有 `test_seg` 和 `test_ram` 两个大测试，
+//! 分别对应对下层的 `Segment` 的接口和上层 `RangeActionMap` ，
+//! 但其实内部包含了关于其他接口的各种测试
+//! 
+//! 注意，这个测试只测数据结构对区间的维护是否正确，
+//! 不包含实际内存分配的部分
+
 mod resource;
 use std::fmt::Debug;
 
@@ -9,6 +19,10 @@ pub use pteflags::*;
 
 use range_action_map::{ArgsType, IdentType, RangeActionMap, Segment};
 
+/// 测试用的下层区间结构。
+/// 
+/// 它模拟了内核中相同权限的连续(虚拟)内存段(如`\kernel`内的`VmArea`)
+/// 但没有实际对应到内粗
 pub struct Seg {
     pub start: usize,
     pub end: usize,
@@ -43,6 +57,10 @@ impl Seg {
     }
 }
 
+/// 实现下层接口。
+/// 
+/// 注意其中的 `args` 是被忽略的，因为在内核中这个参数是用来传递额外的信息的，如页表地址。
+/// 而在这个测试中只包含数据结构的测试，没有实际内存分配，所以没有页表部分。
 impl Segment for Seg {
     fn remove(&mut self, _args: ArgsType) {
         self.frames.clear();
@@ -63,6 +81,7 @@ impl Segment for Seg {
     }
 }
 
+/// find 接口测试
 pub fn test_find(ram: &mut RangeActionMap<Seg>, pos: usize) -> bool {
     println!("try find seg include {pos}");
     if let Some(seg) = ram.find(pos) {
@@ -74,6 +93,7 @@ pub fn test_find(ram: &mut RangeActionMap<Seg>, pos: usize) -> bool {
     }
 }
 
+/// mmap_fixed 接口的包装，实际测试在下面
 pub fn test_mmap_fixed(ram: &mut RangeActionMap<Seg>, start: usize, end: usize, flag: PTEFlags) {
     ram.mmap_fixed(start, end, || {
         println!("mapped to {} {}", start, end);
@@ -81,6 +101,7 @@ pub fn test_mmap_fixed(ram: &mut RangeActionMap<Seg>, start: usize, end: usize, 
     });
 }
 
+/// mmap_fixed 接口的包装，实际测试在下面
 pub fn test_mmap_anywhere(ram: &mut RangeActionMap<Seg>, hint: usize, len: usize, flag: PTEFlags) {
     ram.mmap_anywhere(hint, len, |start| {
         let end = start + len;
@@ -89,6 +110,7 @@ pub fn test_mmap_anywhere(ram: &mut RangeActionMap<Seg>, hint: usize, len: usize
     });
 }
 
+/// 获取对应位置的 PTEFlags，用于测试
 pub fn test_get_flag_at(ram: &mut RangeActionMap<Seg>, pos: usize) -> PTEFlags {
     ram.find(pos).unwrap().flags
 }
@@ -96,6 +118,7 @@ pub fn test_get_flag_at(ram: &mut RangeActionMap<Seg>, pos: usize) -> PTEFlags {
 fn main() {}
 
 #[test]
+/// 对上层接口的总体测试
 fn test_ram() {
     let mut ram = RangeActionMap::<Seg>::new(ArgsType::default());
     test_mmap_fixed(&mut ram, 0x3000, 0x7000, PTE_RU());
@@ -131,6 +154,7 @@ fn test_ram() {
 }
 
 #[test]
+/// 对下层同权限区间的总体测试
 fn test_seg() {
     let mut seg = Seg::new(5, 10, PTE_RU());
     seg.shrink_to_left(8, ArgsType::default());
--- range-action-map/src/lib.rs
+++ range-action-map/src/lib.rs
@@ -1,4 +1,67 @@
-//！ 一个区间树结构，用于提供 mmap / munmap / mprotect 时对区间的操作
+//! # RangeAction Map
+//! 
+//! 一个区间树结构，用于提供 mmap / munmap / mprotect 时对区间的操作。
+//! 
+//! ## 使用
+//! 
+//! 本项目有 `std` 环境和 `no_std` 两个选项。
+//! 
+//! 如需在 `std` 环境使用，直接在 `Cargo.toml` 引入即可；
+//! 如需在内核中使用，则需要选择：
+//! ```
+//! range-action-map = { path = "https://github.com/scPointer/maturin/tree/memory-area-mod/range-action-map", default-features = false }
+//! ```
+//! 
+//! ## 测试
+//! 
+//! 本项目来自 `https://github.com/scPointer/maturin`。
+//! 
+//! 其中 crate 源码在 `https://github.com/scPointer/maturin/tree/master/range-action-map`，
+//! 对这个 crate 本身的单元测试在  `https://github.com/scPointer/maturin/tree/master/range-action-map-test`。
+//! 
+//! 单元测试本身只包含对数据结构本身的测试，不涉及页表和内存分配。实际在内存中的应用见下
+//! 
+//! ## 应用
+//! 
+//! 主要在 `https://github.com/scPointer/maturin/kernel` 模块中，
+//! **下面的路径都以这个模块为当前路径做描述**
+//! 
+//! - 在 `src/memory/areas/mod.rs` 中，描述了内核里的 `VmArea` 如何实现这个模块的 `trait Segment`
+//! - 在 `src/memory/vmm.rs` 中，描述了内核里的 `MemorySet` 如何使用这个模块的 `RangeActionMap`
+//! - - 以及如何使用 `VmArea`
+//! - 在 `Cargo.toml` 中，描述了如何引入本模块
+//! 
+//! ## 对外提供接口
+//! 
+//! RangeActionMap 主要提供以下操作：
+//! - `unmap(start, end)`：拆分/缩短/删除部分现有区间，空出`[start, end)` 这一段。
+//! - `mprotect(start, end)`：修改所有区间与 `[start,end)` 相交的部分的属性。没有被 `[start, end)` 覆盖的区间会被拆分。
+//! - `mmap_fixed(start, end)`：`unmap(start, end)`，并插入一个(用户给定的)新区间在`[start, end)`。
+//! - `mmap_anywhere(hint, len)`：不修改任何区间，寻找一个长为 len 且左端点不小于 `hint` 的空位，并插入一个(用户给定的)新区间，返回插入位置的左端点。
+//! 
+//! 还提供以下接口：
+//! - `find(pos: usize)`：查询一个点是否在某个区间在，如果在，返回它的引用。
+//! - `.iter()` `.iter_mut()`：迭代器支持。
+//! - `impl Debug`：可以用 Debug 属性输出所有区间信息(需要用户提供的底层 `SegmentType` 实现 `Debug`)。
+//! 
+//! 创建：
+//! - `new(args: ArgsType)`
+//! 
+//! 
+//! ## 需要用户提供的接口和约定
+//! 
+//! `RangeActionMap` 需要一个实现 `trait Segment` 的类型，至少实现删除、拆分、修改三个操作：
+//! - `remove()`：删除这段区间
+//! - `split(pos)`：从`pos`位置把当前区间拆成两段区间(pos 参数为全局的绝对位置而非区间内位置)
+//! - `modify(new_flags)`：修改区间的属性
+//! 
+//! 一些约定：
+//! - 删除区间时需要用户底层结构完成返还页帧、修改页表等操作，但不需要 `Drop` 结构本身
+//! - 其中每个区间有一个 usize 大小的可修改的属性，在用于内存管理时，它一般是 PTEFlags
+//! - - (尽管这个结构只需要u8，但我们希望这个属性至少可以放下一个 raw_pointer 以便扩展其他用途)。
+//! - **此外，`RangeActionMap`创建时要求传入一个 `ArgsType`，它实际上是一个 usize。这个值会在每次操作时传递给底层区间**
+//!
+ 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(btree_drain_filter)]
 
@@ -49,6 +112,32 @@ pub use defs::{ArgsType, IdentType, LOWER_LIMIT, UPPER_LIMIT};
 /// - `modify(new_flags)`：修改区间的属性
 /// 
 /// **此外，`RangeActionMap`创建时要求传入一个 `ArgsType`，它实际上是一个 usize。这个值会在每次操作时传递给底层区间**
+/// 
+/// # Example
+/// 
+/// ```
+/// # use range_action_map::{RangeActionMap, Segment, IdentType, ArgsType};
+/// /// 定义一个区间结构，内部只保存左右端点
+/// struct Seg(usize, usize);
+/// let mut ram = RangeActionMap::<Seg>::new(ArgsType::default());
+/// /// 分配一段区间，注意区间是**左闭右开**的
+/// ram.mmap_fixed(0x3000, 0x7000, || { Seg(0x3000, 0x7000) });
+/// assert!(ram.find(0x2111).is_none());
+/// assert!(ram.find(0x3000).is_some());
+/// assert!(ram.find(0x5678).is_some());
+/// assert!(ram.find(0x7000).is_none());
+/// 
+/// /// 实现 Segment
+/// impl Segment for Seg {
+///     fn remove(&mut self, args: ArgsType) {}
+///     fn modify(&mut self, new_flag: IdentType, args: ArgsType) {}
+///     fn split(&mut self, pos: usize, args: ArgsType) -> Self {
+///         let right_end = self.1;
+///         self.1 = pos;
+///         Self(pos, right_end)
+///     }
+/// }
+/// ```
 /// 
 pub struct RangeActionMap<SegmentType: Segment> {
     pub segments: BTreeMap<usize, RangeArea<SegmentType>>,
