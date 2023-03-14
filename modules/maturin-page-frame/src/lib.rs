7473b2f208f1a616126fceff1a6a86ec55f7856d
闭浩扬 <bhy18@mails.tsinghua.edu.cn>
2023-03-15 01:23:32 +0800

    add description for page-frame

--- modules/maturin-page-frame/src/lib.rs
+++ modules/maturin-page-frame/src/lib.rs
@@ -1,4 +1,63 @@
-//! 页帧分配器
+//! # 页帧分配器
+//! 
+//! 一个 RAII 的页帧，包装了由 Bitmap 实现的分配器。
+//! 
+//! ## 使用
+//! 
+//! 本项目有 `std` 环境和 `no_std` 两个选项。
+//! 
+//! 如需在 `std` 环境使用，直接在 `Cargo.toml` 引入即可；
+//! 如需在内核中使用，则需要选择：
+//! ```ignore
+//! range-action-map = { default-features = false }
+//! ```
+//! 
+//! 使用步骤：
+//! 
+//! ```
+//! // 1. 设置 Config 函数
+//! # use maturin_page_frame::{PageFrameConfig, Frame};
+//! struct MyPageFrameConfig;
+//! impl PageFrameConfig for MyPageFrameConfig {
+//! // 这里可以选择重写任意函数，也可以完全不重写
+//! }
+//! type MyFrame = Frame<MyPageFrameConfig>;
+//! 
+//! // 2. 在内核启动时，向页帧分配器中加入任意个物理地址段(后续也可随时添加)
+//! unsafe {
+//!     MyFrame::init(vec![0x80000000..0x80002000, 0xffff5000..0xffff6000]);
+//! }
+//! 
+//! // 3. 在上面的初始化过程后，任意核在任意时候可以通过 `Frame::new()` 尝试获取页帧，
+//! // 然后保存起来(`Frame` 会在 drop 时自动释放)
+//! let mut frames: Vec<MyFrame> = vec![];
+//! for i in 0..3 {
+//!     let frame = MyFrame::new().unwrap();
+//!     frames.push(frame);
+//! }
+//! 
+//! // 4. 检查分配情况
+//! assert_eq!(frames[0].size(), 0x1000);
+//! assert_eq!(frames[0].start_paddr(), 0x80000000);
+//! assert_eq!(frames[1].start_paddr(), 0x80001000);
+//! assert_eq!(frames[2].start_paddr(), 0xffff5000);
+//! assert!(MyFrame::new().is_none());
+//! frames.clear();
+//! assert!(MyFrame::new().is_some());
+//! 
+//! ```
+//! ## 测试
+//! 
+//! 本项目来自 `https://github.com/scPointer/maturin`。
+//! 
+//! 其中 crate 源码在 `https://github.com/scPointer/maturin/tree/master/range-action-map`，
+//! 对这个 crate 本身的单元测试在  `https://github.com/scPointer/maturin/tree/master/range-action-map-test`。
+//! 
+//! 单元测试本身只包含对数据结构本身的测试，不涉及页表和内存分配。实际在内存中的应用见下
+//! 
+//! ## 应用参考
+//! 
+//! 见 `https://github.com/scPointer/maturin/blob/master/kernel/src/memory/allocator/frame.rs`
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
